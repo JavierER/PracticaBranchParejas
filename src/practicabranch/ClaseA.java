70461ec5b8be5d10e233ff8e0ba3d0547ffb36f4
nlopezjimenez <nlopezjimenez@atanasoff23.danielcastelao.org>
2016-02-29 10:06:33 +0100

    Commit 2 clasea devB

--- src/practicabranch/ClaseA.java
+++ src/practicabranch/ClaseA.java
@@ -5,6 +5,8 @@
  */
 package practicabranch;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Javii
@@ -12,5 +14,10 @@ package practicabranch;
 public class ClaseA {
     public void aux_metodo1(){
         System.out.println("Commit en branch devB");
+    }
+    public void aux_metodo2(){
+        int nota;
+       nota= Integer.parseInt(JOptionPane.showInputDialog("Introduce nota :"));
+       JOptionPane.showMessageDialog(null,"La nota es de "+nota);
     }
 }
