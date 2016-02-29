fdb375fd7b72ce4c217272ca09a66216748cc6e8
Javii <Javii@Javi>
2016-02-29 09:53:46 +0100

    Primer commit de la master

---
+++ src/practicabranch/PracticaBranch.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicabranch;
+
+/**
+ *
+ * @author Javii
+ */
+public class PracticaBranch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Primer commit en la master");
+    }
+    }
+    
+
